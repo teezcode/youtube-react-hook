8f07fefb370ea33886df4313f414ccc4e1eba604
teezcode <spaschal43@gmail.com>
2020-12-11 03:22:31 +0100

    we about to push

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,36 @@
-import  React from 'react';
+import  React, {useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
+import useVideos from '../Hooks/UseVideos';
 
-class App extends React.Component {
+const App=()=>{
+    
+    const [selectedVideo, setSelectedVideo] = useState(null);
+    const [videos,search] = useVideos('davido')
 
-    state={ videos: [], selectedVideo: null };
+    useEffect(()=>{
+        setSelectedVideo(videos[0]);
+    }, [videos])
 
-    componentDidMount(){
-        this.onTermSubmit('phyno')
-    }
-
-    onTermSubmit = async (term)=>{
-      const response = await youtube.get('/search',{
-            params:{
-             q:term
-            }
-        });
-        this.setState({ 
-            videos: response.data.items,
-            selectedVideo:response.data.items[0]
-        });
-        
-    };
-
-onVideoSelect=(video)=>{
-    this.setState({selectedVideo:video});
-}
-
-
-    render() {
-        return (
-            <div className="ui container">
-                <SearchBar onFormSubmit={this.onTermSubmit} />
-                    <div className="ui grid">
-                        <div className="ui row">
-                            <div className="eleven wide column">
-                                <VideoDetail video={this.state.selectedVideo} />
-                            </div>
-                            <div className="five wide column">
-                                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
-                            </div>  
-                        </div>     
-                    </div>
-            </div>
-        );
-    }
+    return (
+        <div className="ui container">
+            <SearchBar onFormSubmit={search} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList 
+                                onVideoSelect={(video)=>setSelectedVideo(video)} 
+                                videos={videos}
+                            />
+                        </div>  
+                    </div>     
+                </div>
+        </div>
+    );
 }
 
 export default App;
